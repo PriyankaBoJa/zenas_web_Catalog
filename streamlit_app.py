ff2f0ac3b041cedc435ba66283ec4e74b13430d3
PriyankaBoJa <162777016+PriyankaBoJa@users.noreply.github.com>
2024-03-13 22:37:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ info_dataframe = session.table("catalog_for_website").select(col('color_or_style
 # put the dafta into a dataframe
 pd_df = info_dataframe.to_pandas()
 st.dataframe(pd_df)
-txt = pd_df.loc[0,'direct_url']
+txt = pd_df.loc[0,'direct_url'].iloc(0)
 st.write(txt)
 info_dataframe['direct_url'][1]
 #st.write(direct_url)
